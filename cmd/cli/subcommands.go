c9e235342953f0cf88b5a7bf09c224634c072415
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-19 05:06:59 +0200

    feat(cmd, cli, subcmd): add subcommands run methods as placeholders and register sub commands in isde of helpers method

--- cmd/cli/subcommands.go
+++ cmd/cli/subcommands.go
@@ -11,7 +11,7 @@ func init() {
 		Use:   "create [name] [email] [age]",
 		Short: "Create a new user",
 		Args:  cobra.ExactArgs(3),
-		//	Run:   call createUser func,
+		Run:   createUser,
 	}
 	userCreateCmd.Flags().StringP("role", "r", "user", "User role")
 
@@ -19,13 +19,13 @@ func init() {
 		Use:   "get [id]",
 		Short: "Get user by ID",
 		Args:  cobra.ExactArgs(1),
-		//Run:   getUser,
+		Run:   getUser,
 	}
 
 	userListCmd := &cobra.Command{
 		Use:   "list",
 		Short: "List all users",
-		//Run:   listUsers,
+		Run:   listUsers,
 	}
 	userListCmd.Flags().IntP("limit", "l", 50, "Maximum users to display")
 
@@ -33,7 +33,7 @@ func init() {
 		Use:   "update [id]",
 		Short: "Update user information",
 		Args:  cobra.ExactArgs(1),
-		//Run:   updateUser,
+		Run:   updateUser,
 	}
 	userUpdateCmd.Flags().StringP("name", "n", "", "New name")
 	userUpdateCmd.Flags().StringP("email", "e", "", "New email")
@@ -43,10 +43,11 @@ func init() {
 		Use:   "delete [id]",
 		Short: "Delete a user",
 		Args:  cobra.ExactArgs(1),
-		//Run:   deleteUser,
+		Run:   deleteUser,
 	}
 
 	// Add user subcommands to user command
+	getUserCommand().AddCommand(userCreateCmd, userGetCmd, userListCmd, userUpdateCmd, userDeleteCmd)
 }
 
 // helper method to get command (to be linked from root.go)
@@ -56,19 +57,51 @@ func getUserCommand() *cobra.Command {
 
 func setupAuthCommands() {
 	authLoginCmd := &cobra.Command{
-		Use: "Login [username]";
+		Use: "Login [username]",
 		Short: "Login to the system",
 		Args:  cobra.ExactArgs(1),
-		//Run: Login
+		Run: login
 	}
 
 	authLogoutCmd := &cobra.Command{
 		Use: "Logout",
 		Short: "Logout the system",
-		//Run: Logout
+		Run: logout,
 	}
+
+	getAuthCommand().AddCommand(authLoginCmd, authLogoutCmd)
 }
 
 func getAuthCommand() *cobra.Command {
 	return &cobra.Command{Use: "auth"} // placeholder
+}
+
+
+// TODO: function to be implemented later
+func createUser(cmd *cobra.Command, args []string) {
+	fmt.Printf("Creating user: %s, %s, %s\n", args[0], args[1], args[2])
+}
+
+func getUser(cmd *cobra.Command, args []string) {
+	fmt.Printf("Getting user: %s\n", args[0])
+}
+
+func listUsers(cmd *cobra.Command, args []string) {
+	fmt.Println("Listing all users")
+}
+
+func updateUser(cmd *cobra.Command, args []string) {
+	fmt.Printf("Updating user: %s\n", args[0])
+}
+
+func deleteUser(cmd *cobra.Command, args []string) {
+	fmt.Printf("Deleting user: %s\n", args[0])
+}
+
+func login(cmd *cobra.Command, args []string) {
+	fmt.Printf("Logging in user: %s\n", args[0])
+}
+
+func logout(cmd *cobra.Command, args []string) {
+	fmt.Println("Logging out")
 }
