f64fe1ea8f6814360234fa46dae9a18f9e8d7ee5
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-19 03:56:39 +0200

    feat(cmd): define root command for project's CLI app using Cobra package

---
+++ cmd/root.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "user-manager",
+	Short: "userManager is a CLI for managing users in memory",
+	Long:  `UserManager CLI - A learning project for Go A complete CRUD application with authentication and concurrency features built for learning Go fundamentals`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to the UserManager CLI")
+		fmt.Println("Use 'user-manager --help' to see available commands")
+	},
+}
