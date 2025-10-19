e7dcb019041a350388e4d2d54894dd4a2bd8a14d
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-19 18:06:56 +0200

    feat(models, user): add function that creates new user instance with generated ID

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,14 +1,27 @@
 package models
 
 import (
-	"time"
 	"github.com/google/uuid"
+	"time"
 )
 
 type User struct {
 	ID        string    `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
+	Age       int       `json:"age"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewUser(name, email string, age int) *User {
+	now := time.Now()
+	return &User{
+		ID:        uuid.NewString(),
+		Name:      name,
+		Email:     email,
+		Age:       age,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
 }
