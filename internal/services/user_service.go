1dab1d47c2be90c08bd10c7f428f67d35eae43ab
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-20 12:43:07 +0200

    feat(service, user): implement interface functions for service and abstraction of persistence layer

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -14,3 +14,56 @@ func NewService(storage storage.Storage) *Service {
 		store: storage,
 	}
 }
+
+func (s *Service) CreateUser(name, email string, age int) (*models.User, error) {
+	user := models.NewUser(name, email, age)
+
+	if err := s.store.Create(user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *Service) GetUser(id string) (*models.User, error) {
+	return s.store.GetById(id)
+}
+
+func (s *Service) GetAll() ([]*models.User, error) {
+	return s.store.GetAll()
+}
+
+func (s *Service) Update(id string, name, email string, age int) (*models.User, error) {
+
+	user, err := s.store.GetById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	updatedUser := &models.User{
+		ID:        user.ID,
+		Name:      name,
+		Email:     email,
+		Age:       age,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+
+	if ok := s.store.Update(id, updatedUser); ok != nil {
+		return nil, ok
+	}
+
+	return s.store.GetById(id)
+}
+
+func (s *Service) Delete(id string) error {
+	return s.store.Delete(id)
+}
+
+func (s *Service) Exists(id string) bool {
+	return s.store.Exists(id)
+}
+
+func (s *Service) Count() int {
+	return s.store.Count()
+}
