a12ab2de142f0a05c6b3ae6069a73549714ab615
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-25 19:51:16 +0200

    feat(cmd): create auth commands, and attach them to rootCMD

--- cmd/auth.go
+++ cmd/auth.go
@@ -9,8 +9,24 @@ import (
 
 func init() {
 	authCommands := commands.NewAuthCommands()
+
+	setupAuthCommands(authCommands)
 }
 
 func setupAuthCommands(authCommands *commands.AuthCommands) {
-	authCmd.AddCommand()
+	authCmd.AddCommand(
+		&cobra.Command{
+			Use:   "login [username]",
+			Short: "Login to the system",
+			Args:  cobra.ExactArgs(1),
+			Run:   authCommands.Login,
+		},
+		&cobra.Command{
+			Use:   "logout",
+			Short: "Logout from the system",
+			Run:   authCommands.Logout,
+		},
+	)
+
+	rootCmd.AddCommand(authCmd)
 }
